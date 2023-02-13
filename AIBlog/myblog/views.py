f8c30b9c455259af7ec457a048bef187a72897d7
torres76ru <59732839+torres76ru@users.noreply.github.com>
2023-02-13 22:11:49 +0300

    first commit

---
+++ AIBlog/myblog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+from .forms import PostForm
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    #fields = ('title', 'body')
